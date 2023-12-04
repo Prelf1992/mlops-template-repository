a66becc2c34707aded2ef3505de10c99fc3707e8
Alexander Vance <perphontibecu@gmx.net>
2023-12-04 16:16:58 -0500

    Refactor: Refactor code in Python data loader for better maintainability.

--- src/mlops_pipeline.py
+++ src/mlops_pipeline.py
@@ -428,3 +428,5 @@ if __name__ == "__main__":
 # Simulated change for commit 222 on 2023-11-23 15:49:21
 
 # Simulated change for commit 223 on 2023-11-24 12:10:23
+
+# Simulated change for commit 227 on 2023-12-04 16:16:58
